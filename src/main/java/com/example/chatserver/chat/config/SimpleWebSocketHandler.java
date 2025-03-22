aedac68bd6b7ed08e0f1805ecaa9311ac6c60b6d
hingjae <ljh23270@naver.com>
2025-03-22 21:55:42 +0900

    WebSocket 설정 제거

--- src/main/java/com/example/chatserver/chat/config/SimpleWebSocketHandler.java
+++ src/main/java/com/example/chatserver/chat/config/SimpleWebSocketHandler.java
@@ -1,49 +1,49 @@
-package com.example.chatserver.chat.config;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.*;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
-@Slf4j
-@Component
-public class SimpleWebSocketHandler extends TextWebSocketHandler {
-
-    // 동시에 연결 요청이 왔을 때 thread-safe 해야함.
-    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
-
-    /**
-     * client 메모리에 등록
-     */
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        sessions.add(session);
-        log.info("session connected: {}", session.getId());
-    }
-
-    /**
-     * 사용자에게 메시지 전송
-     */
-    @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String payload = message.getPayload();
-        log.info("receive message: {}", payload);
-        for (WebSocketSession targetSession : sessions) {
-            if (targetSession.isOpen()) {
-                targetSession.sendMessage(new TextMessage(payload));
-            }
-        }
-    }
-
-    /**
-     * 연결이 끊기면 client를 메모리에서 삭제
-     */
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        sessions.remove(session);
-        log.info("session disconnected: {}", session.getId());
-    }
-}
+//package com.example.chatserver.chat.config;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.socket.*;
+//import org.springframework.web.socket.handler.TextWebSocketHandler;
+//
+//import java.util.Set;
+//import java.util.concurrent.ConcurrentHashMap;
+//
+//@Slf4j
+//@Component
+//public class SimpleWebSocketHandler extends TextWebSocketHandler {
+//
+//    // 동시에 연결 요청이 왔을 때 thread-safe 해야함.
+//    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
+//
+//    /**
+//     * client 메모리에 등록
+//     */
+//    @Override
+//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+//        sessions.add(session);
+//        log.info("session connected: {}", session.getId());
+//    }
+//
+//    /**
+//     * 사용자에게 메시지 전송
+//     */
+//    @Override
+//    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+//        String payload = message.getPayload();
+//        log.info("receive message: {}", payload);
+//        for (WebSocketSession targetSession : sessions) {
+//            if (targetSession.isOpen()) {
+//                targetSession.sendMessage(new TextMessage(payload));
+//            }
+//        }
+//    }
+//
+//    /**
+//     * 연결이 끊기면 client를 메모리에서 삭제
+//     */
+//    @Override
+//    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+//        sessions.remove(session);
+//        log.info("session disconnected: {}", session.getId());
+//    }
+//}
--- src/main/java/com/example/chatserver/chat/config/WebSocketConfig.java
+++ src/main/java/com/example/chatserver/chat/config/WebSocketConfig.java
@@ -1,24 +1,24 @@
-package com.example.chatserver.chat.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-@Configuration
-@RequiredArgsConstructor
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    private final SimpleWebSocketHandler simpleWebSocketHandler;
-
-    // 웹소켓을 처리할 핸들러를 등록해
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // /connect url 로 websocket 연결 요청이 들어오면, handler 클래스가 처리함.
-        registry.addHandler(simpleWebSocketHandler, "/connect")
-                .setAllowedOrigins("http://localhost:5173"); // websocket 프로토콜용 cors 설정.
-
-    }
-}
+//package com.example.chatserver.chat.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//@EnableWebSocket
+//public class WebSocketConfig implements WebSocketConfigurer {
+//
+//    private final SimpleWebSocketHandler simpleWebSocketHandler;
+//
+//    // 웹소켓을 처리할 핸들러를 등록해
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        // /connect url 로 websocket 연결 요청이 들어오면, handler 클래스가 처리함.
+//        registry.addHandler(simpleWebSocketHandler, "/connect")
+//                .setAllowedOrigins("http://localhost:5173"); // websocket 프로토콜용 cors 설정.
+//
+//    }
+//}
